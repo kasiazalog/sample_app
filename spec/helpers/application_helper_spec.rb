c8a1190f825d89f0ddd4d6af17c67b58f7959186
Kasia Zalog <kasia.zalog@gmail.com>
2014-01-24 21:33:25 +0100

    Make a basic User model (including secure passwords)

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -11,7 +11,7 @@ describe ApplicationHelper do
 			expect(full_title("foo")).to match(/^Ruby on Rails Tutorial Sample App/)
 		end
 
-		id "should not include a bar for the home page" do
+		it "should not include a bar for the home page" do
 			expect(full_title("")).not_to match(/\|/)
 		end
 	end
